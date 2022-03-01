fff418f759c64427aa12c57672954e829affc621
SKAYHF <rocks_fire@mail.ru>
2022-03-01 23:38:59 +0300

    append push

--- main.c
+++ main.c
@@ -32,11 +32,81 @@ void test_reserve_zeroCapacity(){
 }
 
 void test_reserve_toZeroVector_emptyVector() {
-    vector v = createVector(5);
-    size_t newCapasity = 0;
-    reserve(&v,newCapasity);
+    vector v = createVector(7);
+    pushBack(&v, 6);
+    reserve(&v, 5);
+    assert(v.capacity == 5);
+    assert(v.size == 1);
+    deleteVector(&v);
 }
 
+void test_reserve_increasingTheSize() {
+    vector v = createVector(8);
+    pushBack(&v,8);
+    pushBack(&v,6);
+    reserve(&v,3);
+    assert(v.capacity == 3);
+    assert(v.size == 3);
+    deleteVector(&v);
+}
+
+void test_clear_vector(){
+    vector v = createVector(2);
+    pushBack(&v,2);
+    pushBack(&v,3);
+    clear(&v);
+    assert(v.capacity == 0);
+    assert(v.size == 0);
+    deleteVector(&v);
+}
+
+void test_isEmpty_toFitNotVectorWithCapacityMoreSize() {
+    vector  v = createVector(5);
+    pushBack(&v, 6);
+    pushBack(&v, 7);
+    shrinkToFit(&v);
+    assert(isEmpty(&v));
+    deleteVector(&v);
+}
+
+void test_isEmpty_vector(){
+    vector v = createVector(3);
+    shrinkToFit(&v);
+    assert(isEmpty(&v));
+    deleteVector(&v);
+}
+
+void test_isEmply_fullVector(){
+    vector  v = createVector(5);
+    pushBack(&v, 3);
+    shrinkToFit(&v);
+    assert(isEmpty(&v));
+    deleteVector(&v);
+}
+
+void test_pushBack_Vector(){
+    vector v = createVector(0);
+    pushBack(&v,3);
+    assert(v.size == 1);
+    assert(v.capacity == 1);
+    deleteVector(&v);
+
+}
+
+void test_pushBack_fullVector(){
+    vector v = createVector(3);
+    pushBack(&v,1);
+    assert(v.size == 2);
+    assert(v.capacity == 3);
+}
+
+void test_pushBack_notEmptyVector(){
+    vector v = createVector(9);
+    pushBack(&v, 7);
+    assert(v.size == 2);
+    assert(v.capacity == 9);
+    deleteVector(&v);
+}
 
 
 
