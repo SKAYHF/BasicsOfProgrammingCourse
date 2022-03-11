f5b87eb64961b025b918e143cbd6928bccbf9b29
SKAYHF <rocks_fire@mail.ru>
2022-03-12 00:59:25 +0300

    added features

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -1,13 +1,57 @@
 #ifndef UNTITLED1_STRING__H
 #define UNTITLED1_STRING__H
 
+#define MAX_STRING_SIZE 100
+#define MAX_N_WORDS_IN_STRING 100
+#define MAX_WORD_SIZE 20
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include "memory.h"
 
 typedef struct WordDescriptor {
     char *begin; // позиция начала слова
     char *end; // позиция первого символа, после последнего символа слова
 } WordDescriptor;
+
+typedef struct BagOfWords {
+    WordDescriptor  words[MAX_N_WORDS_IN_STRING];
+    size_t size;
+}BagOfWords;
+
+typedef enum WordBeforeFirstWordWithAReturnCode {
+    FIRST_WORD_WITH_A,
+    NOT_FOUND_A_WORD_WITH_A,
+    WORD_FOUND,
+    EMPTY_STRING
+} WordBeforeFirstWordWithAReturnCode;
+
+static BagOfWords bag;
+static BagOfWords bag2;
+
+size_t strlen(const char *begin);
+
+char *find(char *begin, char *end, int ch);
+
+char *findNonSpace(char *begin);
+
+char *findSpace(char *begin);
+
+char *findNonSpaceReverse(char *rbegin, const char *rend);
+
+char *findSpaceReverse(char *rbegin, const char *rend);
+
+int strcmp(const char *lhs, const char *rhs);
+
+char *copy(const char *beginSource, const char *endSource, char *beginDestination);
+
+char *copyIf_(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int));
+
+char *copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int));
+
+char *getEndOfString(char *begin);
+
 
 #endif
